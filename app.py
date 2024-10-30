ed32450be2d2c69e29ae66af7c451e0f935a0978
mikeedel <105459492+mikeedel@users.noreply.github.com>
2024-10-30 17:49:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
+import pandas as pd
 from joblib import load
 import pathlib
 from fastapi.middleware.cors import CORSMiddleware
@@ -39,8 +40,7 @@ class OutputData(BaseModel):
 
 @app.post('/score', response_model = OutputData)
 def score(data:InputData):
-    model_input = np.array([v for k,v in data.dict().items()]).reshape(1,-1)
-    result = model.predict_proba(model_input)[:,-1]
+    model_input = pd.DataFrame([data.dict()])
+    result = model.predict_proba(model_input)[:,-1].item()
 
     return {'score':result}
-
